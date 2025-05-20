4ccbe1fc67c36ba39ee8a71a5511e8f38f62f2e2
Sajay1 <sajaysuresh307@gmail.com>
2025-05-20 13:09:01 +0530

    signup

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -33,9 +33,10 @@ useEffect(() => {
     return(
     <div className="flex flex-col justify-center items-center bg-[url('https://imgs.search.brave.com/AyvS9nTktqchL6N57dj0Pr8CM84_yln4Wa0NFsyzHlg/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDgz/NDk1MjEwL3Bob3Rv/L2NvbmNlcnQtY3Jv/d2QuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPVpzNTk0bThm/NUxKN0RxSlRLMnk2/Vi1Tb2p3dmtFUXRn/aU5Pc20wQTJzTmM9')] bg-cover bg-center min-h-screen">
       <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <h1 className='font-bold text-[28px] text-gray dark:text-white flex lfex-col'>SignUp</h1>
-        <form onSubmit={handleSubmit} className='flex flex-col gap-y-4'>
-          <div className='flex flex-space-x-2'>
+              <h1 className='text-[28px] font-bold text-gray dark:text-white flex flex-col mb-6 text-center'>SignUp</h1>
+
+               <form className='flex flex-col' onSubmit={handleSubmit}>
+          <div className='flex flex-space-x-4 mb-4'>
              <label htmlFor="username" className='text-[18px] text-gray dark:text-white'>Name</label>
           <input type="text" id="username" value={name} onChange={(e)=>setName(e.target.value)} className='w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
           </div>
@@ -58,8 +59,9 @@ useEffect(() => {
             <option value="User">User</option>
              </select>
           </div>
-         </form>     
-    </div>
+         </form>   
+
+      </div>  
     </div>
 )
 }
