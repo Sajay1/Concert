2dcd5cf84a08d2fbd3071e8be599aadfe0828f55
Sajay1 <sajaysuresh307@gmail.com>
2025-05-20 17:57:30 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,81 @@
-import { NavLink } from "react-router-dom";
-import "../styles/Navbar.css";
-import React from "react";
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="mx-auto text-white font-bold text-2xl">
-                Concert
-            </div>
-                <div className="nav-items ">
-                        <NavLink to="/signup" className="nav-item">
-                        Home
-                    </NavLink>
-                    <NavLink to="/signup" className="nav-item">
-                        Signup
-                    </NavLink>
-                    <NavLink to="/login" className="nav-item">
-                        Login
-                    </NavLink>
-            </div>
-        </nav>
-    )
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'Signup', href: '/signup', current: false },
+  { name: 'Login', href: '/login', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Navbar;
+export default function Example() {
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex shrink-0 items-center">
+              <span
+                className="h-8 w-auto"
+              >
+                <h1 className='items-center mx-auto font-bold text-[30px] text-gray dark:text-white'>Concert</h1>
+              </span>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium',
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+            
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  )
+}
