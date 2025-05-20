b298bb1e8ca1525b5d4c25592bd1588228790811
Sajay1 <sajaysuresh307@gmail.com>
2025-05-20 10:56:32 +0530

    signup

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -34,14 +34,31 @@ useEffect(() => {
     <div className='flex min-h-screen items-center justify-center bg-slate-100 dark:bg-slate-900'>
       <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
         <h1 className='text-[28px] font-bold text-white mb-6 text-center'>SignUp</h1>
-        <div className='flex flex-col gap-y-4'>
-        <form onSubmit={handleSubmit}>
-          <div className=''>
-          <label htmlFor="username">Name</label>
-          <input type="text" />
+        <form onSubmit={handleSubmit} className='flex flex-col gap-y-4'>
+          <div className='flex flex-col'>
+             <label htmlFor="username" className='text-[18px]'>Name</label>
+          <input type="text" id="username" value={name} onChange={(e)=>setName(e.target.value)} className='w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
           </div>
-        </form>     
-        </div>
+          <div className='flex flex-col'>
+             <label htmlFor="email" className='text-[18px] text-green-100'>Email</label>
+          <input type="text" id="username" value={email} onChange={(e)=>setEmail(e.target.value)} className='w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
+          </div>
+          <div className='flex flex-col'>
+             <label htmlFor="password" className='text-[18px] text-green-100'>Password</label>
+          <input type="text" id="username" value={password} onChange={(e)=>setPassword(e.target.value)} className='w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
+          </div>
+          <div className='flex flex-col'>
+             <label htmlFor="confirmpassowrd" className='text-[18px] text-green-100'>Confirm Password</label>
+          <input type="text" id="username" value={confirmpassword} onChange={(e)=>setConfirmPassword(e.target.value)} className='w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500' required/>
+          </div>
+          <div className='flex flex-col'>
+             <label htmlFor="username" className='text-[18px] text-green-100'>Role</label>
+             <select name="role" id="role" className='text-white border-white border-2 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500' value={role} onChange={(e)=>setRole(e.target.value)}> 
+            <option value="Admin">Admin</option>
+            <option value="User">User</option>
+             </select>
+          </div>
+         </form>     
     </div>
     </div>
 )
