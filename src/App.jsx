091bfb3289a521b9946228920d4c5333b39293f9
Sajay1 <sajaysuresh307@gmail.com>
2025-05-21 10:02:23 +0530

    none

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from './components/Navbar';
 import Signup from './components/SignupForm';
 import Login from './components/Login';
 import { div } from 'framer-motion/client';
-import { Route } from 'react-router-dom';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 
 export default function App() {
 
   return(
   <div>
-      <Navbar/>
-  <Signup/>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+                <Route path='/signup' element={<Signup/>}/>
+      </Routes>
   </div>
 );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,8 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon , XMarkIcon } from '@heroicons/react/24/outline'
+import {  Link } from 'react-router-dom';
 
-const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Signup', href: '/signup', current: false },
-  { name: 'Login', href: '/login', current: false },
-]
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -35,19 +32,8 @@ export default function Navbar() {
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
-                    className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium',
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
+                  <Link to='/signup'>Signup</Link>
+                  <Link to='/'>Login</Link>
               </div>
             </div>
           </div>
